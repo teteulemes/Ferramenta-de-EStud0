88b5a739d882b88d440821b3a7c24af44bc62679
teteulemes <matheus.ariel.lemes@escola.pr.gov.br>
2025-08-13 07:38:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 function criaCartao(categoria, pergunta, resposta) {
-  console.log( categoria, pergunta, resposta)
+ let container = document.getElementById('container')
+let cartao = document.createElement('article')
+cartao.className = 'cartao'
+
+  cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>Culinária</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>Quem faz a melhor lasanha do mundo</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+         <p>O Matheus Lemes</p>
+    </div>
+    </div>
+`
 }
